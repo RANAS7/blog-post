b9f2a50d6a8ff38d84367040fbebeaf6096ba5f2
RANAS7 <deepakrana.dr537@gmail.com>
2024-08-20 13:49:13 +0545

    push all code

---
+++ everestFitness/src/main/java/com/msp/everestFitness/everestFitness/EverestFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.msp.everestFitness.everestFitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverestFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverestFitnessApplication.class, args);
+	}
+
+}
---
+++ everestFitness/src/test/java/com/msp/everestFitness/everestFitness/EverestFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msp.everestFitness.everestFitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverestFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
